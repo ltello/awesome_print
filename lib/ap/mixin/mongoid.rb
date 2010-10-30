e704ddad3531f1d8e8ea83d8672e58401b56dcc6
Lorenzo Tello <ltello@belor.jazztel.es>
2010-10-31 00:53:03 +0200

    Now it works. At least for me

--- lib/ap/mixin/mongoid.rb
+++ lib/ap/mixin/mongoid.rb
@@ -6,45 +6,45 @@
 module AwesomePrintMongoid
 
   def self.included(base)
-    puts "#{base.class} #{base} ", "included module AwesomePrintMongoid\n"
+    puts "#{base} available"
     
     base.send :alias_method, :printable_without_mongoid, :printable
     base.send :alias_method, :printable, :printable_with_mongoid
   end
 
-  # Add ActiveRecord class names to the dispatcher pipeline.
+  # Add Mongoid to the dispatcher pipeline.
   #------------------------------------------------------------------------------
   def printable_with_mongoid(object)
     
-      puts "Object inside awesome: #{object.inspect}"
     printable = printable_without_mongoid(object)
-        puts "printable"+printable.inspect
     if printable == :self
-      if object.is_a?(ActiveRecord::Base)
+      if object.respond_to?(:fields)
         printable = :mongoid_instance
       end
-    elsif printable == :class and object.ancestors.include?(ActiveRecord::Base)
+    elsif printable == :class and !object.respond_to?(:to_model)
       printable = :mongoid_class
     end
     printable
   end
 
-  # Format ActiveRecord instance object.
+  # Format Mongoid instance object.
   #------------------------------------------------------------------------------
   def awesome_mongoid_instance(object)
-    data = object.class.column_names.inject(ActiveSupport::OrderedHash.new) do |hash, name|
-      hash[name.to_sym] = object.send(name) if object.has_attribute?(name) || object.new_record?
-      hash
-    end
+    keys = (object.fields.keys + object.attributes.keys + object.associations.keys).uniq
+      data = keys.inject(ActiveSupport::OrderedHash.new) do |hash, name|
+        hash[name.to_sym] = object.send(name) if object.respond_to?(name)
+        hash
+      end
     "#{object} " + awesome_hash(data)
   end
 
-  # Format ActiveRecord class object.
+  # Format Mongoid class object.
   #------------------------------------------------------------------------------
   def awesome_mongoid_class(object)
-    if object.respond_to?(:columns)
-      data = object.columns.inject(ActiveSupport::OrderedHash.new) do |hash, c|
-        hash[c.name.to_sym] = c.type
+    if object.respond_to?(:fields)
+      keys = (object.fields.keys + object.associations.keys).uniq
+      data = keys.inject(ActiveSupport::OrderedHash.new) do |hash, c|
+        hash[c.to_sym] = object.fields.keys.include?(c) ? object.fields[c].options.to_s : object.associations[c].options.to_s
         hash
       end
       "class #{object} < #{object.superclass} " << awesome_hash(data)
